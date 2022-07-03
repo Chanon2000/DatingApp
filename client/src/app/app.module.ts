60cd635277d1ff41ed9bb7d949b02e911b41c384
Chanon <chanonchanon@arisecorpn.com>
2022-07-03 12:03:47 +0700

    se5: Adding a home page

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,11 +7,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component'
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown'
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { HomeComponent } from './home/home.component'
 
 @NgModule({
   declarations: [ // component ที่จะใช้ใน project
-    AppComponent, NavComponent
+    AppComponent, NavComponent, HomeComponent
   ],
   imports: [
     BrowserModule,
