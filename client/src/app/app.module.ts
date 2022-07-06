a9f61a3d199da664f0c00b811c204e8e8119987b
Chanon <chanonchanon@arisecorpn.com>
2022-07-06 23:19:42 +0700

    se7: (79) Testing errors in the client

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -16,10 +16,11 @@ import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component'
 import { ToastrModule } from 'ngx-toastr';
 import { SharedModule } from './_modules/shared.module';
+import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
 
 @NgModule({
   declarations: [
-    AppComponent, NavComponent, HomeComponent, RegisterComponent, MemberListComponent, MemberDetailComponent, ListsComponent, MessagesComponent
+    AppComponent, NavComponent, HomeComponent, RegisterComponent, MemberListComponent, MemberDetailComponent, ListsComponent, MessagesComponent, TestErrorsComponent
   ],
   imports: [
     BrowserModule,
