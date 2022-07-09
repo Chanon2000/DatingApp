1719965855605c36c0a7e0b41fe91717b2d5c0c7
Chanon <chanonchanon@arisecorpn.com>
2022-07-09 09:18:21 +0700

    se8: (87) Adding a DateTime extension to calculate age

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using API.Extensions;
 
 namespace API.Entities
 {
@@ -9,7 +10,7 @@ namespace API.Entities
         public string UserName { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
-        public DateTime DateOfBirth { get; set; }
+        public DateTime DateOfBirth { get; set; } // ให้ user บอก วันเกิดดีกว่าบอกอายุเพราะอายุมันเปลี่ยนทุกปี แต่วันเกิดจะเหมือนเดิม
         public string KnownAs { get; set; }
         public DateTime Created { get; set; } = DateTime.Now;
         public DateTime LastActive { get; set; } = DateTime.Now;
@@ -20,5 +21,10 @@ namespace API.Entities
         public string City { get; set; }
         public string Country { get; set; }
         public ICollection<Photo> Photos { get; set; }
+        // สร้าง method ใน entity class
+        public int GetAge()
+        {
+            return DateOfBirth.CalculateAge();
+        }
     }
 }
