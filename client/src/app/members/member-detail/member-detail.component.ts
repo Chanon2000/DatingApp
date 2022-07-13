25db81becb917606a55beb6eca64630097135bf5
Chanon <chanonchanon@arisecorpn.com>
2022-07-13 15:45:14 +0700

    se9: 110. Routing to the detailed page

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Member } from 'src/app/_models/member';
+import { MembersService } from 'src/app/_services/members.service';
 
 @Component({
   selector: 'app-member-detail',
@@ -6,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./member-detail.component.css']
 })
 export class MemberDetailComponent implements OnInit {
+  member: Member;
 
-  constructor() { }
+  constructor(private memberService: MembersService, private route:ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.loadMember();
+    // knownAs ของ member ขึ้นที่ template แต่ มันเกิด error ว่า ...undefined (reading 'knownAs')
+    // เพราะว่า เมื่อ angular create template เสร็จ ข้อมูล user มันยังไม่มา ดังนั้นจึงแก้ด้วยการใส่ *ngIf="member" ที่ template
+  }
+
+  loadMember() {
+    this.memberService.getMember(this.route.snapshot.paramMap.get('username')).subscribe(member => {
+      this.member = member;
+    })
   }
 
 }
