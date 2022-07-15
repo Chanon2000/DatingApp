ac96ff1b18069ccf2e1b48a31066e28d1470a3d9
Chanon <chanonchanon@arisecorpn.com>
2022-07-15 09:16:03 +0700

    se10: Persisting the changes in the API

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using API.Data;
 using API.DTOs;
@@ -35,6 +36,24 @@ namespace API.Controllers
         public async Task<ActionResult<MemberDto>> GetUser(string username)
         {
             return await _userRepository.GetMemberAsync(username);
+        }
+        // Put ใช่เพื่อ update resource 
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+        {
+            // เราไม่ใว่ใจ user ที่ยิง request มา เพื่อเอาค่า username แต่เราต้องการ username จาก token ที่ authenticate มา
+            // ClaimTypes.NameIdentifier คือ claim ที่เราให้แต่ละ user ใส่ token
+            // User ตรงนี้จะเก็บข้อมูล claim, identity ต่างๆ
+            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value; // เอา username จาก token มาใส่
+            var user = await _userRepository.GetUserByUsernameAsync(username);
+
+            _mapper.Map(memberUpdateDto, user); // map ไปที่ user
+
+            _userRepository.Update(user); // update โดย entity framework
+
+            if (await _userRepository.SaveAllAsync()) return NoContent();
+
+            return BadRequest("Failed to update user");
         }
     }
 }
