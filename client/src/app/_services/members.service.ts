530fc7b7f843b996a7b85e9dede7a7ebfb061001
Chanon <chanonchanon@arisecorpn.com>
2022-07-15 21:44:57 +0700

    se10: Using the service to store state

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { Member } from '../_models/member';
 
@@ -9,18 +10,34 @@ import { Member } from '../_models/member';
 })
 export class MembersService {
   baseUrl = environment.apiUrl;
+  // service เป็น singletons ดังนั้น servide จึงเหมาะมากในการเก็บ state ของ application ของเรา
+  // ความจริงมี state management solutions อื่นๆ เช่น Redux ใส่ลง application แต่ angular ไม่จำเป็นเพราะมี service อยู่แล้ว
+  members: Member[] = [];
 
   constructor(private http: HttpClient) { }
 
   getMembers(): Observable<Member[]> {
-    return this.http.get<Member[]>(this.baseUrl + 'users');
+    if (this.members.length > 0) return of(this.members); // ถ้ามีข้อมูล member ใช้เลย ไม่ต้องยิงเรียกข้อมูลอีกที
+    return this.http.get<Member[]>(this.baseUrl + 'users').pipe( //เพื่อเก็บข้อมูล member ลง service
+      map(members => {
+        this.members = members; 
+        return members; // members ตรงนี้ก็เป็น obv นะ (อยู่ใน map)
+      })
+    );
   }
 
   getMember(username: string) {
+    const member = this.members.find(x => x.username === username); // ถ้าไม่เจออะไรเลย find จะ return undefined
+    if (member !== undefined) return of(member);
     return this.http.get<Member>(this.baseUrl + 'users/' + username);
   }
 
   updateMember(member: Member) {
-    return this.http.put(this.baseUrl + 'users', member);
+    return this.http.put(this.baseUrl + 'users', member).pipe( // เพื่อที่เมื่อ update แล้วจะได้ไม่ต้องเป็นยิงเพื่อดึงข้อมูลที่ update แล้วมา
+      map(() => {
+        const index = this.members.indexOf(member);
+        this.members[index] = member;
+      })
+    )
   }
 }
