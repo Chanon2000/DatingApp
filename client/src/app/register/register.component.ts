928dea1f62bb2c6856c258e3fd84dea8d40835c6
Chanon <chanonchanon@arisecorpn.com>
2022-07-17 15:49:12 +0700

    se12: Adding custom validators

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
@@ -8,7 +8,7 @@ import { AccountService } from '../_services/account.service';
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent implements OnInit {1
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
   registerForm: FormGroup; // group ของ form control
@@ -24,8 +24,19 @@ export class RegisterComponent implements OnInit {
       username: new FormControl('Hello', Validators.required),
       password: new FormControl('', [Validators.required, 
         Validators.minLength(4), Validators.maxLength(8)]), // เมื่อต้องการใส่มากกว่า 1 validator ให้ใส่ []
-      confirmPassword: new FormControl('', Validators.required),
+      confirmPassword: new FormControl('', [Validators.required, this.matchValues('password')]),
     })
+    this.registerForm.controls.password.valueChanges.subscribe(() => {
+      this.registerForm.controls.confirmPassword.updateValueAndValidity(); // เพื่อทุกครั้งเมื่อ password มีการเปลี่ยนแปลงค่า ก็ให้ไป check ตัวว่ามันตรงกับ confirmPassword มั้ย
+    })
+  }
+
+  matchValues(matchTo: string): ValidatorFn { // ValidatorFn คือ method นี้ return validator function
+    return (control: AbstractControl) => { // control นี้คือ confirmPassword เพราะเราจะเอา Validator นี้ไปแนบที่ confirmPassword control
+      return control?.value === control?.parent?.controls[matchTo].value 
+        ? null : {isMatching: true};
+      // isMatching เป็น make up name field เพื่อเมื่อเวลามันไม่ match กัน มันจะไปเพิ่ม field isMatching ที่ error ของ control ที่แทบ validatior ตัวนี้(ซึ่งทำให้เราสามารถรู้ได้ว่ามันไม่ match)
+    }
   }
 
   register() {
