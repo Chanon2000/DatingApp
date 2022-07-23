25ad015595c6557f6322791f7b21c3704bb55d0a
Chanon <chanonchanon@arisecorpn.com>
2022-07-23 17:58:30 +0700

    se14: Implementing the likes repository

--- API/Data/LikesRepository.cs
+++ API/Data/LikesRepository.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using API.DTOs;
 using API.Entities;
+using API.Extensions;
 using API.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Data
 {
@@ -16,19 +18,46 @@ namespace API.Data
             _context = context;
         }
 
-        public Task<UserLike> GetUserLike(int sourceUserId, int likeUserId)
+        public async Task<UserLike> GetUserLike(int sourceUserId, int likeUserId)
         {
-            throw new NotImplementedException();
+            return await _context.Likes.FindAsync(sourceUserId, likeUserId); // เพราะ 2 key นี้รวมกันถึงจะได้ primary key (เพราะเป็น join table) 
         }
 
-        public Task<IEnumerable<LikeDto>> GetUserLikes(string predicate, int userId)
+        public async Task<IEnumerable<LikeDto>> GetUserLikes(string predicate, int userId)
         {
-            throw new NotImplementedException();
+            var users = _context.Users.OrderBy(u => u.UserName).AsQueryable(); // เหมือนใส่ OrderBy เตรียมไว้ก่อน (ไม่จำเป็นต้อง OrderBy หลังจาก select เพราะยังไงมันก็ไป execute พร้อมกันที่ return)
+            var likes = _context.Likes.AsQueryable();
+
+            if (predicate == "liked")
+            {
+                likes = likes.Where(like => like.SourceUserId == userId);
+                users = likes.Select(like => like.LikedUser); // จะได้ users จาก like table (LikedUser ถ้าไปดูที่ UserLike class มันจะคือ AppUser นั้นแหละ)
+                // ใส่ลง users query
+            }
+
+            if (predicate == "likedBy")
+            {
+                likes = likes.Where(like => like.LikedUserId == userId);
+                users = likes.Select(like => like.SourceUser); // จะได้ list of users ที่ liked user ที่กำลัง login อยู่
+            }
+            
+            return await users.Select(user => new LikeDto 
+            // เราไม่ใช่ autoMapper เราจะใช้แค่ manual select statement เพื่อ project ตรงๆลง LikeDto // เนื่องจาก prop มันไม่เยอะ เลยคิดว่าถ้าใช้ automapper ก็หน้าจะเขียน code ประมาณนี้
+            {
+                Username = user.UserName,
+                KnownAs = user.KnownAs,
+                Age = user.DateOfBirth.CalculateAge(),
+                PhotoUrl = user.Photos.FirstOrDefault(p => p.IsMain).Url,
+                City = user.City,
+                Id = user.Id
+            }).ToListAsync();
         }
 
-        public Task<AppUser> GetUserWithLikes(int userId)
+        public async Task<AppUser> GetUserWithLikes(int userId)
         {
-            throw new NotImplementedException();
+            return await _context.Users
+                .Include(x => x.LikedUsers)
+                .FirstOrDefaultAsync(x => x.Id == userId);
         }
     }
 }
