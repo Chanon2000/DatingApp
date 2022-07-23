380a1df635f360a686a9aba2cb716ebd24f18c5d
Chanon <chanonchanon@arisecorpn.com>
2022-07-23 16:53:03 +0700

    se14: Adding a likes repository

---
+++ API/DTOs/LikeDto.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.DTOs
+{
+    public class LikeDto // จะ return ข้อมูลของ user ที่ like แค่นี้พอ
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public int Age { get; set; }
+        public string KnownAs { get; set; }
+        public string PhotoUrl { get; set; }
+        public string City { get; set; }
+    }
+}
