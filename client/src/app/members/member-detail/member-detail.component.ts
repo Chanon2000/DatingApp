b580f90e71d0b4a356fd169341abbfca5ebccc7b
Chanon <chanonchanon@arisecorpn.com>
2022-07-24 20:29:29 +0700

    se15: Using query params

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -13,7 +13,7 @@ import { MessageService } from 'src/app/_services/message.service';
   styleUrls: ['./member-detail.component.css']
 })
 export class MemberDetailComponent implements OnInit {
-  @ViewChild('memberTabs') memberTabs: TabsetComponent;
+  @ViewChild('memberTabs', {static: true}) memberTabs: TabsetComponent;
   member: Member;
   galleryOptions: NgxGalleryOptions[];
   galleryImages: NgxGalleryImage[];
@@ -28,6 +28,14 @@ export class MemberDetailComponent implements OnInit {
 
   ngOnInit(): void {
     this.loadMember();
+    
+    // สามารถ subscribe queryParams ได้
+    this.route.queryParams.subscribe(params => {
+      params.tab ? this.selectTab(params.tab) : this.selectTab(0); // เขียนแบบ ternary operator
+    })
+    // มันจะ error ว่า tab undifind นั้นเป็นเพราะว่า คุณจะเห็นที่บรรทัดแรกของ template จะมีเงื่อนไขคือ *ngIf="member" นั้นหมายความว่า #memberTabs ที่ template จะไม่ถูกสร้างจนกว่า member จะมีค่า
+    // การใส่ {static: true} ไปที่ ViewChild ซึ่งก็ไม่ได้ช่วงแก้ปัญหานี้เพราะมันยังเร็วไม่พอ แต่มันทำให้เราใช้ dynamic version ของ your child
+    // วิธีที่ทำให้ error นี้หายคือ เอา *ngIf="member" ออก แต่ก็ยังทำให้เกิด error ที่จุดอื่นอยู่ นั้นก็คือ member จะ undifind แทน ซึ่งคุณก็สามารถแก้ปัญหาได้ด้วยการใส่ ? ในทุกที่ แต่ก็มันทำให้ดูไม่ค่อย clean เท่าใหร่ เราเลยจะใช้ resolvers แทน
 
     this.galleryOptions = [
       {
@@ -64,6 +72,10 @@ export class MemberDetailComponent implements OnInit {
     this.messageService.getMessageThread(this.member.username).subscribe(messages => {
       this.messages = messages;
     })
+  }
+
+  selectTab(tabId: number) {
+    this.memberTabs.tabs[tabId].active = true; // tabs เป็น property ใน TabDirective
   }
 
   // ทำให้มัน loadMessages ตอนที่คลิกที่ Messages Tab (วิธีก่อนหน้ามันจะ loadMessage หลังจาก loadMember เสร็จเลย ทำให้การ loading มีพฤติกรรมแปลกๆ)
