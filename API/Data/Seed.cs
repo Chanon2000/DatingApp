04b2550b70519bb1e43aa2e31187ca5146256768
Chanon <chanonchanon@arisecorpn.com>
2022-07-28 14:35:29 +0700

    se16: Adding roles to the app

--- API/Data/Seed.cs
+++ API/Data/Seed.cs
@@ -13,7 +13,8 @@ namespace API.Data
 {
     public class Seed
     {
-        public static async Task SeedUsers(UserManager<AppUser> userManager) // ใช้ userManager เพื่อสร้าง user แทน
+        public static async Task SeedUsers(UserManager<AppUser> userManager, 
+            RoleManager<AppRole> roleManager) // ใช้ userManager เพื่อสร้าง user แทน
         {
             if (await userManager.Users.AnyAsync()) return; // userManager มันก็มี Users เหมือนกัน // Users ให้เราสามารถเข้าถึง user table ได้
 
@@ -21,12 +22,34 @@ namespace API.Data
             var users = JsonSerializer.Deserialize<List<AppUser>>(userData);
             if (users == null) return; // คุณลืมเขียน if นี้ใน session เก่าๆ
 
+            var roles = new List<AppRole>
+            {
+                new AppRole{Name = "Member"},
+                new AppRole{Name = "Admin"},
+                new AppRole{Name = "Moderator"},
+            };
+
+            foreach (var role in roles)
+            {
+                await roleManager.CreateAsync(role);
+            }
+
             foreach (var user in users)
             {
                 user.UserName = user.UserName.ToLower();
                 await userManager.CreateAsync(user, "Pa$$w0rd");
                 // "Pa$$w0rd" เราทำการ hardcode password // และเนื่องจากคุณปิด RequireNonAlphanumeric ของ password ไปแล้ว ทำให้เราสามารถใส่ password แบบนี้ไม่ซับซ้อนแบบนี้ได้
+                await userManager.AddToRoleAsync(user, "Member");
             }
+
+            // สร้าง admin
+            var admin = new AppUser
+            {
+                UserName = "admin"
+            };
+
+            await userManager.CreateAsync(admin, "Pa$$w0rd");
+            await userManager.AddToRolesAsync(admin, new[] {"Admin", "Moderator"});
 
             // await context.SaveChangesAsync(); // เนื่องจาก userManager จัดการเรื่อง SaveChangesAsync ให้แล้ว
         }
