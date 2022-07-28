edb8464a270eac88717196ff222686101fbc39f2
Chanon <chanonchanon@arisecorpn.com>
2022-07-28 09:49:47 +0700

    se16: Configuring the startup class

--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -1,5 +1,8 @@
 using System.Text;
+using API.Data;
+using API.Entities;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
@@ -10,6 +13,21 @@ namespace API.Extensions
     {
         public static IServiceCollection AddIdentityServices(this IServiceCollection services, IConfiguration config)
         {
+            // services.AddIdentity
+            // ถ้าเราทำแบบ MVC ที่ client serve ด้วย dotnet server แบบนั้นเราจะใช้สิ่งที่เรียกว่า "razor pages"
+            // สิ่งนี้จะให้ full setup เลย เช่น page ที่ต้องการ, cookie based authentication เพราะทุกอย่าง generate โดย dotnet server
+            // แต่ที่เราใช้คือ single page application โดย angular แล้วใช้ token based authorization
+            services.AddIdentityCore<AppUser>(opt => 
+            {
+                // ใส่ option
+                opt.Password.RequireNonAlphanumeric = false; // ปิดการ RequireNonAlphanumeric ออก
+            })
+                .AddRoles<AppRole>()
+                .AddRoleManager<RoleManager<AppRole>>() // อย่าลืมใส่ RoleManager type ก่อนด้วย แล้วค่อยใส่ AppRole type ซึ่งเป็น type ที่เราจะใช้
+                .AddSignInManager<SignInManager<AppUser>>()
+                .AddRoleValidator<RoleValidator<AppRole>>() // พวก type เช่น RoleValidator, RoleManager, SignInManager ที่ใส่เข้าก่อน type ของคุณ เป็น type ของ Services เอง ที่บังคับให้คุณต้องใส่ก่อน
+                .AddEntityFrameworkStores<DataContext>(); // ทำการ setup tables ต่างๆ ของidentity
+
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 .AddJwtBearer(options =>
                 {
