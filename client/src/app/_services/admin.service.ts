3c9e5e23941f680006f1776e9c8b90a184bf440a
Chanon <chanonchanon@arisecorpn.com>
2022-07-29 09:50:28 +0700

    se16: Editing roles part two

--- client/src/app/_services/admin.service.ts
+++ client/src/app/_services/admin.service.ts
@@ -14,4 +14,8 @@ export class AdminService {
   getUsersWithRoles() {
     return this.http.get<Partial<User[]>>(this.baseUrl + 'admin/users-with-roles');
   }
+
+  updateUserRoles(username, roles) {
+    return this.http.post(this.baseUrl + 'admin/edit-roles/' + username + '?roles=' + roles, {});
+  }
 }
