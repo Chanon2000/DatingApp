ce3a97f49fdffdb358f0788d8a7d488a73d1e866
Chanon <chanonmixmay2543@hotmail.com>
2022-08-04 13:26:43 +0700

    se18: 247. Tidying up the member message component

--- client/src/app/members/member-messages/member-messages.component.ts
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -1,9 +1,11 @@
-import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Message } from 'src/app/_models/message';
 import { MessageService } from 'src/app/_services/message.service';
 
 @Component({
+  changeDetection: ChangeDetectionStrategy.OnPush, // เพื่อแก้ปัญหา error ที่เกิดขึ้นเมื่อคุณเพิ่มเรื่อง scrolling เข้าไป
+  // NG0100: ExpressionChangedAfterItHasBeenCheckedError
   selector: 'app-member-messages',
   templateUrl: './member-messages.component.html',
   styleUrls: ['./member-messages.component.css']
